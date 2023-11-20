72accb7cfc41617f5ee67687660fa6f6c373b930
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-11-20 17:55:02 +0200

    Rewrite everything to make it more clear and workable

--- technologies_scraper/technologies_scraper/spiders/technologies_spider.py
+++ technologies_scraper/technologies_scraper/spiders/technologies_spider.py
@@ -1,82 +1,110 @@
 import time
 
 import scrapy
+from scrapy import Selector
 from scrapy.exceptions import CloseSpider
 from scrapy.http import Response
-from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TechnologiesSpider(scrapy.Spider):
     name = "technologies"
     allowed_domains = ["robota.ua"]
-    start_urls = ["https://robota.ua/zapros/python-developer/ukraine"]
+    main_url = "https://robota.ua/zapros/python-developer/ukraine"
 
-    def __init__(self, *args, **kwargs) -> None:
-        super(TechnologiesSpider, self).__init__(*args, **kwargs)
-        self.driver = webdriver.Chrome()
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.driver = Chrome()
 
-    def parse(self, response: Response, **kwargs):
+    def close(self, reason: str) -> None:
+        self.driver.quit()
+        super().close(self, reason)
+
+    def start_requests(self) -> scrapy.Request:
         page_number = 0
 
         while True:
             page_number += 1
-            page_link = self.start_urls[0] + f"?page={page_number}"
-            yield from self.parse_single_page(page_link)
+            url_to_parse = self.main_url + f"?page={page_number}"
+            html_content = self.get_full_html_page(url_to_parse)
 
-    def parse_single_page(self, page_link: str):
-        self.driver.get(page_link)
+            yield scrapy.Request(
+                url=url_to_parse,
+                callback=self.parse,
+                meta={"html_content": html_content},
+            )
 
-        if (self.driver.find_elements(By.CSS_SELECTOR, "h3.santa-typo-h3")[0]
-                .text == "За вашим запитом поки немає вакансій"):
-            raise CloseSpider("Condition met")
+    def parse(self, response: Response, **kwargs):
+        html_content = response.meta["html_content"]
+        selector = Selector(text=html_content)
+        job_cards = selector.css("div.santa-min-h-0")
 
-        self.scroll_to_bottom(speed=10)
-
-        for job_card in self.driver.find_elements(
-            By.CSS_SELECTOR, "div.santa--mb-20"
-        ):
+        for job_card in job_cards:
+            parsed_job_card = self.parse_job_card(job_card)
             yield {
-                "job_title": job_card.find_element(
-                    By.CSS_SELECTOR, "h2.santa-typo-h3"
-                ).text,
-                "link": job_card.find_element(By.CSS_SELECTOR, "a.santa-mb-20")
-                .get_attribute("href"),
+                "job_title": parsed_job_card["job_title"],
+                "job_link": parsed_job_card["job_link"],
+                "job_description": parsed_job_card["job_description"],
             }
 
-    def closed(self) -> None:
-        self.driver.quit()
-
-    def scroll_to_bottom(self, speed: int = 1) -> None:
-        time.sleep(7)
-        total_height = self.driver.execute_script(
-            "return Math.max(document.body.scrollHeight, "
-            "document.body.offsetHeight, "
-            "document.documentElement.clientHeight, "
-            "document.documentElement.scrollHeight, "
-            "document.documentElement.offsetHeight);"
+    def parse_job_card(self, job_card: Selector) -> dict[str, str]:
+        job_title = job_card.css("h2.santa-typo-h3::text").get()
+        job_link = (
+            "https://robota.ua"
+            + job_card.css("a.santa-mb-20::attr(href)").get()
         )
-        prev_height = 0
+        job_description = self.get_job_description(job_link)
+        return {
+            "job_title": job_title,
+            "job_link": job_link,
+            "job_description": job_description,
+        }
+
+    def get_job_description(self, job_link: str) -> str:
+        self.driver.get(job_link)
+        WebDriverWait(self.driver, timeout=10).until(
+            lambda driver_inside: driver_inside.execute_script(
+                'return document.readyState == "complete"'
+            )
+        )
+        html_content = self.driver.page_source
+        selector = Selector(text=html_content)
+        job_description = selector.css("#description-wrap").get()
+        return job_description
+
+    def get_full_html_page(self, url: str) -> str:
+        self.driver.get(url)
+        WebDriverWait(self.driver, timeout=10).until(
+            lambda driver_inside: driver_inside.execute_script(
+                'return document.readyState == "complete"'
+            )
+        )
+
+        if self.driver.find_element(By.CSS_SELECTOR, "h3.santa-typo-h3").text == "За вашим запитом поки немає вакансій":
+            raise CloseSpider("No jobs found")
+
+        self.scroll_to_bottom(self.driver)
+        html_content = self.driver.page_source
+        return html_content
+
+    @staticmethod
+    def scroll_to_bottom(driver: Chrome) -> None:
+        scroll_increment = 500
+        current_scroll = 0
+        sleep_time = 0.25
 
         while True:
-            step_size = total_height // speed
+            driver.execute_script(f"window.scrollBy(0, {scroll_increment});")
+            time.sleep(sleep_time)
+            current_scroll += scroll_increment
 
-            for i in range(prev_height, total_height, step_size):
-                self.driver.execute_script("window.scrollTo(0, {});".format(i))
-                time.sleep(0.1)
-
-            current_height = self.driver.execute_script(
+            if current_scroll >= driver.execute_script(
                 "return Math.max(document.body.scrollHeight, "
                 "document.body.offsetHeight, "
                 "document.documentElement.clientHeight, "
                 "document.documentElement.scrollHeight, "
                 "document.documentElement.offsetHeight);"
-            )
-
-            if current_height == total_height:
+            ):
                 break
-
-            prev_height = total_height
-            total_height = current_height
-
-        time.sleep(2)
