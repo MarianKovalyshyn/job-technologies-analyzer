09c9c3b51385dcca853bd1b21586219e0f2dd4a8
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-11-20 23:12:01 +0200

    Add read_file and count_technologies_in_job_description functions

---
+++ technologies_scraper/technologies_scraper/utils.py
@@ -0,0 +1,35 @@
+import json
+
+
+def count_technologies_in_job_description(
+    job_description: str,
+    technologies_count: dict[str, int],
+    possible_technologies: list[str],
+) -> None:
+    if not job_description:
+        return
+
+    for technology in possible_technologies:
+        if technology in job_description.lower():
+            technologies_count[technology] += 1
+
+
+def read_file(
+    file_path: str,
+    possible_technologies: list[str],
+) -> dict[str, int]:
+    with open(file_path, "r", encoding="utf-8") as json_file:
+        jobs = json.load(json_file)
+
+    technologies_count = {
+        technology: 0 for technology in possible_technologies
+    }
+
+    for job in jobs:
+        count_technologies_in_job_description(
+            job_description=job["job_description"],
+            technologies_count=technologies_count,
+            possible_technologies=possible_technologies,
+        )
+
+    return technologies_count
