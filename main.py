74bce044da7455a14a7f54e06bdd066704bf9401
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-11-20 23:48:11 +0200

    Add visualization of top 10 technologies

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from technologies_scraper.technologies_scraper.utils import (
     read_json_file,
     write_csv_file,
 )
-
+from technologies_visualization.utils import visualize_data
 
 file_path = "technologies_scraper/technologies.json"
 technologies_counted = read_json_file(
@@ -11,3 +11,4 @@ technologies_counted = read_json_file(
     possible_technologies=possible_technologies,
 )
 write_csv_file(technologies_counted=technologies_counted)
+visualize_data()
