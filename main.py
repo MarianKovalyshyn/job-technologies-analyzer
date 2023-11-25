1c3631360b8583a6c04a066dd576eb7c894c6695
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-11-25 14:53:05 +0200

    Refactor code - make it more readable and independent

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from config import possible_technologies
-from technologies_scraper.technologies_scraper.utils import (
-    read_json_file,
-    write_csv_file,
-)
+from technologies_scraper.technologies_scraper.utils import get_data
 from technologies_visualization.utils import visualize_data
 
+
 file_path = "technologies_scraper/technologies.json"
-technologies_counted = read_json_file(
-    file_path=file_path,
-    possible_technologies=possible_technologies,
-)
-write_csv_file(technologies_counted=technologies_counted)
+get_data(file_path=file_path)
 visualize_data()
--- technologies_scraper/technologies_scraper/utils.py
+++ technologies_scraper/technologies_scraper/utils.py
@@ -1,42 +1,44 @@
 import csv
 import json
 
+from config import possible_technologies
+
 
 def count_technologies_in_job_description(
     job_description: str,
     technologies_count: dict[str, int],
-    possible_technologies: list[str],
+    technologies: list[str],
 ) -> None:
     if not job_description:
         return
 
-    for technology in possible_technologies:
+    for technology in technologies:
         if technology in job_description.lower():
             technologies_count[technology] += 1
 
 
 def read_json_file(
     file_path: str,
-    possible_technologies: list[str],
+    technologies: list[str],
 ) -> dict[str, int]:
     with open(file_path, "r", encoding="utf-8") as json_file:
         jobs = json.load(json_file)
 
     technologies_count = {
-        technology: 0 for technology in possible_technologies
+        technology: 0 for technology in technologies
     }
 
     for job in jobs:
         count_technologies_in_job_description(
             job_description=job["job_description"],
             technologies_count=technologies_count,
-            possible_technologies=possible_technologies,
+            technologies=technologies,
         )
 
     return technologies_count
 
 
-def write_csv_file(technologies_counted: dict[str, int]):
+def write_csv_file(technologies_counted: dict[str, int]) -> None:
     with open("technologies_count.csv", "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=["technology", "count"])
         writer.writeheader()
@@ -46,3 +48,11 @@ def write_csv_file(technologies_counted: dict[str, int]):
                 for technology, count in technologies_counted.items()
             ]
         )
+
+
+def get_data(file_path: str) -> None:
+    technologies_counted = read_json_file(
+        file_path=file_path,
+        technologies=possible_technologies,
+    )
+    write_csv_file(technologies_counted=technologies_counted)
